952ba6f002e6c55cb17461836b5049fe14a0b2ce
CorneliusOsei2 <oseiowusuboatengcornelius6@gmail.com>
2023-01-03 20:21:48 -0500

    Init frontend with tailwind

---
+++ app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './Components/Navbar/Navbar'
+
+const App: React.FunctionComponent<{}> = () => {
+    return (
+        <div className="App" >
+            <Navbar></Navbar>
+        </div >
+    );
+}
+
+export default App;
